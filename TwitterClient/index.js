5b12abf1d038fbb5ae0e175b4974cb9d051e92c3
WINXAS <novokuyama1019@gmail.com>
2018-07-22 14:24:26 +0900

    20180722

--- TwitterClient/index.js
+++ TwitterClient/index.js
@@ -1,15 +1,33 @@
-var remote = require('electron').remote;
-var BrowserWindow = remote.BrowserWindow;
-var dialog = remote.dialog;
-function hello() {
-    var options = {
-        title: 'ダイアログのタイトル',
-        type: 'info',
-        buttons: ['OK', 'Cancel'],
-        message: 'メッセージ',
-        detail: 'hello'
-    };
-    var win = BrowserWindow.getFocusedWindow();
-    dialog.showMessageBox(win, options);
-}
-//# sourceMappingURL=index.js.map
+/// <reference path="node_modules/electron/electron.d.ts" />
+var electron = require('electron');
+var BrowserWindow = electron.BrowserWindow;
+var app = electron.app;
+var MyApplication = /** @class */ (function () {
+    function MyApplication(app) {
+        var _this = this;
+        this.app = app;
+        this.mainWindow = null;
+        this.onWindowAllClosed = function () {
+            if (process.platform != 'darwin') {
+                _this.app.quit();
+            }
+        };
+        this.onReady = function () {
+            _this.mainWindow = new BrowserWindow({
+                width: 800,
+                height: 400,
+                minWidth: 500,
+                minHeight: 200,
+                acceptFirstMouse: true,
+                titleBarStyle: 'hidden'
+            });
+            _this.mainWindow.on('closed', function () {
+                _this.mainWindow = null;
+            });
+        };
+        this.app.on('window-all-closed', this.onWindowAllClosed);
+        this.app.on('ready', this.onReady);
+    }
+    return MyApplication;
+}());
+var myapp = new MyApplication(app);
--- TwitterClient/index.ts
+++ TwitterClient/index.ts
@@ -1,7 +1,37 @@
-const app = require('electron').remote.require('app');
+/// <reference path="node_modules/electron/electron.d.ts" />
 
-function tweet(){
+const electron = require('electron');
+const BrowserWindow = electron.BrowserWindow;
+const app = electron.app;
 
-    app.tweet(document.forms["test"].elements["data"].value);
+class MyApplication {
+    mainWindow: Electron.BrowserWindow = null;
 
+    constructor(public app: Electron.App){
+        this.app.on('window-all-closed', this.onWindowAllClosed);
+        this.app.on('ready', this.onReady);
+    }
+
+    onWindowAllClosed = () => {
+        if(process.platform != 'darwin'){
+            this.app.quit();
+        }
+    }
+
+    onReady = () => {
+        this.mainWindow = new BrowserWindow({
+            width: 800,
+            height: 400,
+            minWidth: 500,
+            minHeight: 200,
+            acceptFirstMouse: true,
+            titleBarStyle: 'hidden'
+        });
+
+        this.mainWindow.on('closed', () => {
+            this.mainWindow = null;
+        });
+    }
 }
+
+const myapp = new MyApplication(app);
