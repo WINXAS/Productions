dd0f77897d563b5d0020d9d608671f37f7aba80a
WINXAS <novokuyama1019@gmail.com>
2019-07-31 02:25:39 +0900

    vuetify導入

--- FavoritePlayer/vtuber-watcher/src/background.ts
+++ FavoritePlayer/vtuber-watcher/src/background.ts
@@ -18,7 +18,7 @@ function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({ width: 800, height: 600, webPreferences: {
     nodeIntegration: true
-  } })
+  } });
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
@@ -31,8 +31,8 @@ function createWindow () {
   }
 
   win.on('closed', () => {
-    win = null
-  })
+    win = null;
+  });
 }
 
 // Quit when all windows are closed.
@@ -40,17 +40,17 @@ app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -59,13 +59,13 @@ app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
-      await installVueDevtools()
+      await installVueDevtools();
     } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
+      console.error('Vue Devtools failed to install:', e.toString());
     }
   }
-  createWindow()
-})
+  createWindow();
+});
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
@@ -74,10 +74,10 @@ if (isDevelopment) {
       if (data === 'graceful-exit') {
         app.quit()
       }
-    })
+    });
   } else {
     process.on('SIGTERM', () => {
-      app.quit()
-    })
+      app.quit();
+    });
   }
 }
